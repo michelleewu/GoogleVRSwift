f92ff57488952065bc0797b1ec9d71ac69100684
Michelle Wu <wum3099@lgsstudent.org>
2019-07-17 11:06:13 -0700

    Initial Commit

---
+++ GoogleVRSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoogleVRSwift
+//
+//  Created by Michelle Wu on 7/17/19.
+//  Copyright © 2019 Michelle Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
